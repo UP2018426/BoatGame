9d134eec6e03267ab44fbe40d21a8f983d2a9041
Its_A_Van <isufranc@gmail.com>
2024-03-29 12:23:55 +0000

    Added Vertex Waves

---
+++ Assets/Scripts/WaterManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+// This will add the appropriate components on the object if they don't already exist
+
+public class WaterManager : MonoBehaviour
+{
+    // This is in charge of updating the position of the vertices on the Vertex Wave
+    // THE WaterManager IS JUST FOR VISUALISATION PURPOSES
+
+    private MeshFilter meshFilter;
+
+    private void Awake()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+    }
+
+    private void Update()
+    {
+        Vector3[] verts = meshFilter.mesh.vertices;
+        for (int i = 0; i < verts.Length; i++)
+        {
+            verts[i].y = WaveManager.instance.CalculateWaveHeight(transform.position + verts[i]);
+        }
+
+        // Need to make sure we recalculate the mesh otherwise it will look flat
+        meshFilter.mesh.vertices = verts;
+        meshFilter.mesh.RecalculateBounds();
+        meshFilter.mesh.RecalculateNormals();
+        meshFilter.mesh.RecalculateTangents();
+    }
+}
---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public static WaveManager instance;
+
+    public float speed      = 1.0f;
+    public float timePlayed = 0.0f;
+    [Range(-2.0f, 2.0f)]
+    public float offset     = 0.0f;
+
+    // Wave properties. At the moment these need to be set up manually.  
+    // TODO: Find a way to find this automatically from the shader
+    public Vector4 waveA = new Vector4(1, 0, 0.5f, 10);
+    public Vector4 waveB = new Vector4(0, 1, 0.25f, 20);
+    public Vector4 waveC = new Vector4(1, 1, 0.15f, 10);
+
+    private void Awake()
+    {
+        // Create a Singleton
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            // Destroy this if another WaveManager already exists
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        // Update the time since start
+        timePlayed += Time.deltaTime * speed;
+    }
+
+    private float GerstnerWave(Vector4 wave, Vector3 point, float time)
+    {
+        // Matching Gerstner Wave calculation to the shader
+        float steepness = wave.z;
+        float wavelength = wave.w;
+        float k = (2 * Mathf.PI) / wavelength;
+        float c = Mathf.Sqrt(9.8f / k);
+        Vector2 d = new Vector2(wave.x, wave.y).normalized;
+        float f = k * (Vector2.Dot(d, new Vector2(point.x, point.z)) - c * time);
+        float a = steepness / k;
+
+        float waveHeight = d.x * (a * Mathf.Cos(f)) + a * Mathf.Sin(f) + d.y * (a * Mathf.Cos(f));
+
+        return waveHeight;
+    }
+
+    // Call this method to get the height of the wave at a certain coordinate
+    public float CalculateWaveHeight(Vector3 point)
+    {
+        float height = 0;
+
+        height += GerstnerWave(waveA, point, timePlayed + offset);
+        height += GerstnerWave(waveB, point, timePlayed + offset);
+        height += GerstnerWave(waveC, point, timePlayed + offset);
+
+        return height;
+    }
+
+}
