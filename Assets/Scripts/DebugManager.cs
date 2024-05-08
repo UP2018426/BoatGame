c6bf15674606d37daca96e12b354caef2cba50c8
UP2018426 <isufranc@gmail.com>
2024-05-08 23:32:08 +0100

    Small fix + Build version 0.1

--- Assets/Scripts/DebugManager.cs
+++ Assets/Scripts/DebugManager.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class DebugManager : MonoBehaviour
 {
     [SerializeField] private bool increaseSpeed = false;
-    [SerializeField, Range(1.0f, 16.0f)] private float timeMultiplier = 1.0f;
+    [SerializeField, Range(1.0f, 15.0f)] private float timeMultiplier = 1.0f;
     private float timeMultiplierLaterFrame = 0.0f;
+
+    [SerializeField] private TextMeshProUGUI currentSpeedText;
+
+    [SerializeField] private string speedText;
+
+    [SerializeField] private Slider slider;
 
     // Update is called once per frame
     void Update()
     {
+        timeMultiplier = slider.value;
+
+        currentSpeedText.text = timeMultiplier.ToString("F2") + speedText;
+        
         if (timeMultiplier != timeMultiplierLaterFrame && increaseSpeed)
         {
             timeMultiplierLaterFrame = timeMultiplier;
